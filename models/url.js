2843b8e36265b462d960d67903170f1ef8c492ff
Tuplondhe Rutwik <rutwiktuplondhe7@gmail.com>
2025-07-08 19:30:09 +0530

    Added JWT Authentication

--- models/url.js
+++ models/url.js
@@ -15,6 +15,10 @@ const urlSchema = new mongoose.Schema({
             type: Number, 
         }
     }],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user", 
+    }
 }, 
     { timestamps: true }
 );
